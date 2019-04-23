f8d8384313dae96a55021dd2de057c8773c25144
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-23 14:19:25 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/CSVParsingModules.pm
@@ -0,0 +1,48 @@
+package Bencher::Scenario::CSVParsingModules;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use File::ShareDir::Tarball qw(dist_dir);
+
+our $scenario = {
+    summary => 'Benchmark CSV parsing modules',
+    modules => {
+        # minimum versions
+        #'Foo' => {version=>'0.31'},
+    },
+    participants => [
+        {
+            module => 'Text::CSV_PP',
+            code_template => 'my $csv = Text::CSV_PP->new({binary=>1}); open my $fh, "<", <filename>; my $rows = []; while (my $row = $csv->getline($fh)) { push @$rows, $row }',
+        },
+        {
+            module => 'Text::CSV_XS',
+            code_template => 'my $csv = Text::CSV_XS->new({binary=>1}); open my $fh, "<", <filename>; my $rows = []; while (my $row = $csv->getline($fh)) { push @$rows, $row }',
+        },
+        {
+            name => 'naive-split',
+            code_template => 'open my $fh, "<", <filename>; my $rows = []; while (defined(my $row = <$fh>)) { chomp $row; push @$rows, [split /,/, $row] }',
+        },
+    ],
+
+    datasets => [
+    ],
+};
+
+my $dir = dist_dir('CSV-Examples')
+    or die "Can't find share dir for CSV-Examples";
+for my $filename (glob "$dir/examples/*bench*.csv") {
+    my $basename = $filename; $basename =~ s!.+/!!;
+    push @{ $scenario->{datasets} }, {
+        name => $basename,
+        args => {filename => $filename},
+    };
+}
+
+1;
+# ABSTRACT:
